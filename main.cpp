4adc7319d2acf874364636f220bb0f1b07f3fd5e
Cristian E. Romero <cristianromero96.cr@gmail.com>
2024-08-08 14:46:16 +0000

    if para prender el led

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ int main()
     DigitalOut LD2(LED2); 
 
     while (true) {
-        LD2 = B1_USER;
+        if (B1_USER==1) 
+        {
+        LD2=1; // prendo el led.
+        }
+        else
+        {
+        LD2=0; //apago el led.
+        }
     }
 }
