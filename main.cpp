a5db8033842f429db82d682b23b74fa84acd539f
Cristian E. Romero <cristianromero96.cr@gmail.com>
2024-08-08 11:13:58 +0000

    Quita de comentarios

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    DigitalIn B1_USER(BUTTON1); //objeto de la clase digitalin
+    DigitalIn B1_USER(BUTTON1);
 
-    DigitalOut LD2(LED2);
+    DigitalOut LD2(LED2); 
 
     while (true) {
         LD2 = B1_USER;
