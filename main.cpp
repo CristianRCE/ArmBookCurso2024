e2a0639897269a5b8d2f6c16da303cc60837f895
Cristian E. Romero <cristianromero96.cr@gmail.com>
2024-08-21 00:10:54 +0000

    Turn on led1

--- main.cpp
+++ main.cpp
@@ -1,23 +1,12 @@
 #include "mbed.h"
 
+
 int main()
 {
     DigitalIn B1_USER(BUTTON1);
-    // DigitalOut Trigger_sensor();
-    // DigitalIn Echo_sensor();
-    DigitalOut LD2(LED2); 
-
+    DigitalOut LD1(LED1);
+    
     while (true){
-        if(B1_USER==0) 
-        {
-        LD2=1; // prendo el led.
-        wait_us(1000000);
-        LD2=0; // prendo el led.
-        wait_us(1000000);
-        }
-        else
-        {
-        LD2=0; //apago el led.
-        }
+        LD1 = B1_USER;
     }
 }
